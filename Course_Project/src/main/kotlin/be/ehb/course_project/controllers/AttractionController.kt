1ae745539942c5b416e0f6c36ed33eee84071053
remygist <remy.gistelinck@student.ehb.be>
2024-04-11 17:00:48 +0200

    setup db

--- Course_Project/src/main/kotlin/be/ehb/course_project/controllers/AttractionController.kt
+++ Course_Project/src/main/kotlin/be/ehb/course_project/controllers/AttractionController.kt
@@ -3,14 +3,12 @@ package be.ehb.course_project.controllers
 import be.ehb.course_project.models.Attraction
 import be.ehb.course_project.services.AttractionService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+
 
 @RestController
 @RequestMapping("index")
+@CrossOrigin
 class AttractionController {
     @Autowired
     lateinit var attractionService: AttractionService
