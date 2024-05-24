487da91cc275aecd85abfcd6ada7cf537f785a23
remygist <remy.gistelinck@student.ehb.be>
2024-05-24 16:29:22 +0200

    TODO: edit & delete category

--- Course_Project_Front_End/src/router/index.js
+++ Course_Project_Front_End/src/router/index.js
@@ -2,12 +2,16 @@ import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeView from '../views/Homepage.vue'
 import AddAttractionView from '../views/AddAttractions.vue'
-import EditAttraction from '@/views/EditAttraction.vue'
+import EditAttraction from '../views/EditAttraction.vue'
+import AllCategories from '../views/AllCategories.vue'
+import EditCategories from '../views/EditCategories.vue'
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/attractions/add', component: AddAttractionView },
-  { path: '/attraction/edit', component: EditAttraction }
+  { path: '/attraction/edit', component: EditAttraction },
+  { path: '/categories', component: AllCategories},
+  { path:	'/category/editCategory', component: EditCategories}
 ]
 
 const router = createRouter({
---
+++ Course_Project_Front_End/src/views/EditCategories.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+    data() {
+        return {
+            categoryTitle: this.$route.query.title,
+            category: null
+        };
+    },
+    mounted() {
+        this.fetchCategory();
+    },
+    methods: {
+        async fetchCategory() {
+            const currentComponent = this;
+
+            fetch(`http://localhost:8080/category/getOne/${this.categoryTitle}`)
+                .then(response => response.json())
+                .then(function (category) {
+                    currentComponent.category = category;
+                    console.log(currentComponent.category);
+                })
+        },
+        submit() {
+            const categoryData = {
+                
+            };
+
+            // console.log(categoryData);
+            // fetch(`http://localhost:8080/index/editAttraction/${this.categoryTitle}`,{
+            //     method: 'PUT',
+            //     headers: {"Content-Type": "application/json"},
+            //     body: JSON.stringify(categoryData)
+            // }).then(function (response){
+            //     console.log(response);
+            // })
+        },
+        // deleteCategory(){
+        //     const currentComponent = this;
+
+        //     fetch(`http://localhost:8080/index/deleteAttraction/${this.attractionName}`,{
+        //         method: 'DELETE',
+        //         headers: {"Content-Type": "application/json"},
+        //     }).then(function (response){
+        //         console.log(response);
+        //     })
+        //     this.$router.push('/');
+            
+        // }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Edit Category</h1>
+        <p>Editing: {{ categoryTitle }}</p>
+       
+    </div>
+</template>
