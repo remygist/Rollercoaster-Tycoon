b3ef71928b6169dd2ea4a6c4ab39734da8c4208f
JordyGovaerts <114074918+JordyGovaerts@users.noreply.github.com>
2024-05-25 20:58:05 +0200

    added end maintenance function

--- Course_Project_Front_End/src/components/Attraction.vue
+++ Course_Project_Front_End/src/components/Attraction.vue
@@ -14,12 +14,24 @@ export default {
         nextMaintenance: String,
         categories: Array,
         maintenances: Array
+    },
+    methods: {
+        endMaintenance(){
+            fetch(`http://localhost:8080/maintenance/finishMaintenance/${this.name}`,{
+                method: 'PUT',
+                headers: {"Content-Type": "application/json"},
+            }).then(function (response){
+                console.log(response);
+            }), window.location.reload();
+        }
+        
     }
 }
 </script>
 
 <template>
     <div class="attraction">
+        <template v-if="inMaintenance">
         <p>Name:{{ name }}</p>
         <p>Capacity: {{ capacity }}</p>
         <p>Date of build: {{ dateOfBuild }}</p>
@@ -31,14 +43,40 @@ export default {
         <p>Categories: <span v-for="(category, index) in categories" :key="index">
                 {{ category.title }},
             </span></p>
-        <p>Previous maintenances: {{ maintenances }}</p>
+        <p>Previous maintenances: <span v-for="(maintenance, index) in maintenances" :key="index">
+                {{ maintenance.reason }},
+            </span></p>
+        <p>Next maintenance due: Ongoing</p>
+        <a :href="onrideVideo" target="_blank">Onride video</a>
+        <img :src="image" :alt="name">
+        <br>
+        <br>
+        <RouterLink :to="{ path: '/attraction/edit', query: { name: name } }" class="btn">Edit Attraction</RouterLink>
+            <button @click="endMaintenance" class="btn">End Maintenance</button>
+        </template>
+        <template v-else>
+            <p>Name:{{ name }}</p>
+        <p>Capacity: {{ capacity }}</p>
+        <p>Date of build: {{ dateOfBuild }}</p>
+        <p>Maintenance frequency: {{ maintenanceFrequency }}</p>
+        <p>Duration: {{ duration }}</p>
+        <p>Minimum height: {{ minHeight }}</p>
+        <p>Speed: {{ speed }}</p>
+        <p>In maintenance: {{ inMaintenance }}</p>
+        <p>Categories: <span v-for="(category, index) in categories" :key="index">
+                {{ category.title }},
+            </span></p>
+        <p>Previous maintenances: <span v-for="(maintenance, index) in maintenances" :key="index">
+                {{ maintenance.reason }},
+            </span></p>
         <p>Next maintenance due: {{ nextMaintenance }}</p>
         <a :href="onrideVideo" target="_blank">Onride video</a>
         <img :src="image" :alt="name">
         <br>
         <br>
         <RouterLink :to="{ path: '/attraction/edit', query: { name: name } }" class="btn">Edit Attraction</RouterLink>
-        <RouterLink :to="{ path: '/maintenance/add', query: { name: name } }" class="btn">Start Maintenance</RouterLink>
+            <RouterLink :to="{ path: '/maintenance/add', query: { name: name } }" class="btn">Start Maintenance</RouterLink>
+        </template>
     </div>
 </template>
 
