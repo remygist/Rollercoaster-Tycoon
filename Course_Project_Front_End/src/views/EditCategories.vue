9648cf43e70abc464e3429c6ed5c4120d793a91a
remygist <remy.gistelinck@student.ehb.be>
2024-05-25 12:52:14 +0200

    edit and delete category works

--- Course_Project_Front_End/src/views/EditCategories.vue
+++ Course_Project_Front_End/src/views/EditCategories.vue
@@ -3,57 +3,48 @@ export default {
     data() {
         return {
             categoryTitle: this.$route.query.title,
+            originalTitle: this.$route.query.title,
             category: null
         };
     },
-    mounted() {
-        this.fetchCategory();
-    },
     methods: {
-        async fetchCategory() {
-            const currentComponent = this;
-
-            fetch(`http://localhost:8080/category/getOne/${this.categoryTitle}`)
-                .then(response => response.json())
-                .then(function (category) {
-                    currentComponent.category = category;
-                    console.log(currentComponent.category);
-                })
-        },
         submit() {
             const categoryData = {
-                
+                title: this.categoryTitle
             };
 
-            // console.log(categoryData);
-            // fetch(`http://localhost:8080/index/editAttraction/${this.categoryTitle}`,{
-            //     method: 'PUT',
-            //     headers: {"Content-Type": "application/json"},
-            //     body: JSON.stringify(categoryData)
-            // }).then(function (response){
-            //     console.log(response);
-            // })
+            console.log(categoryData);
+            fetch(`http://localhost:8080/category/editCategory/${this.originalTitle}`,{
+                method: 'PUT',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(categoryData)
+            }).then(function (response){
+                console.log(response);
+            })
         },
-        // deleteCategory(){
-        //     const currentComponent = this;
+        deleteCategory(){
+            const currentComponent = this;
 
-        //     fetch(`http://localhost:8080/index/deleteAttraction/${this.attractionName}`,{
-        //         method: 'DELETE',
-        //         headers: {"Content-Type": "application/json"},
-        //     }).then(function (response){
-        //         console.log(response);
-        //     })
-        //     this.$router.push('/');
+            fetch(`http://localhost:8080/category/deleteCategory/${this.originalTitle}`,{
+                method: 'DELETE',
+                headers: {"Content-Type": "application/json"},
+            }).then(function (response){
+                console.log(response);
+            })
+            this.$router.push('/');
             
-        // }
+        }
     }
 }
 </script>
 
 <template>
     <div>
-        <h1>Edit Category</h1>
-        <p>Editing: {{ categoryTitle }}</p>
-       
+        <form @submit.prevent="submit">
+                <h2>Edit Attraction Details</h2>
+                <p>Name: <input type="text" v-model="categoryTitle"></p>
+                <button type="submit">Save Changes</button>
+                <button @click="deleteCategory">Delete Category</button>
+            </form>
     </div>
 </template>
