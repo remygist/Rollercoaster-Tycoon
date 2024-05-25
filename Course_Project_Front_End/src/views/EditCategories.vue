6d78f4bb73c008773fae8774608b6c088bae1efb
JordyGovaerts <114074918+JordyGovaerts@users.noreply.github.com>
2024-05-25 19:17:53 +0200

    Category add attraction

--- Course_Project_Front_End/src/views/EditCategories.vue
+++ Course_Project_Front_End/src/views/EditCategories.vue
@@ -4,35 +4,54 @@ export default {
         return {
             categoryTitle: this.$route.query.title,
             originalTitle: this.$route.query.title,
-            category: null
+            category: null,
+            attractionName: null,
+            attractions: []
         };
     },
     methods: {
         submit() {
             const categoryData = {
-                title: this.categoryTitle
+                title: this.categoryTitle,
+                attractions: this.attractions
             };
 
             console.log(categoryData);
-            fetch(`http://localhost:8080/category/editCategory/${this.originalTitle}`,{
+            fetch(`http://localhost:8080/category/editCategory/${this.originalTitle}`, {
                 method: 'PUT',
-                headers: {"Content-Type": "application/json"},
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(categoryData)
-            }).then(function (response){
+            }).then(function (response) {
                 console.log(response);
             })
         },
-        deleteCategory(){
+        deleteCategory() {
             const currentComponent = this;
 
-            fetch(`http://localhost:8080/category/deleteCategory/${this.originalTitle}`,{
+            fetch(`http://localhost:8080/category/deleteCategory/${this.originalTitle}`, {
                 method: 'DELETE',
-                headers: {"Content-Type": "application/json"},
-            }).then(function (response){
+                headers: { "Content-Type": "application/json" },
+            }).then(function (response) {
                 console.log(response);
             })
             this.$router.push('/');
+
+        },
+        addAttraction() {  
+            const categoryData = {
+                categoryName: this.categoryTitle,
+                attractionName: this.attractionName
+            }
+            console.log(categoryData)
+            fetch(`http://localhost:8080/index/addCategory`, {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(categoryData)
+            }).then(function (response) {
+                console.log(response);
+            })
             
+     
         }
     }
 }
@@ -41,10 +60,16 @@ export default {
 <template>
     <div>
         <form @submit.prevent="submit">
-                <h2>Edit Attraction Details</h2>
-                <p>Name: <input type="text" v-model="categoryTitle"></p>
-                <button type="submit">Save Changes</button>
-                <button @click="deleteCategory">Delete Category</button>
-            </form>
+            <h2>Edit Categories Details</h2>
+            <p>Name: <input type="text" v-model="categoryTitle"></p>
+            <button type="submit">Save Changes</button>
+            <button @click="deleteCategory">Delete Category</button>
+            <div>
+                <h3>Add Attraction</h3>
+                <input type="text" v-model="attractionName" placeholder="Attraction Name">
+                <button type="button" @click="addAttraction">Add Attraction</button>
+            </div>
+        </form>
     </div>
+
 </template>
