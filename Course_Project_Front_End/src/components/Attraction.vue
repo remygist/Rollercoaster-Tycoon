44afe78fb9b1bef13373140e93a4074b5802607d
remygist <remy.gistelinck@student.ehb.be>
2024-05-27 00:51:11 +0200

    restructured code for readability

--- Course_Project_Front_End/src/components/Attraction.vue
+++ Course_Project_Front_End/src/components/Attraction.vue
@@ -31,54 +31,31 @@ export default {
 
 <template>
     <div class="attraction">
-        <template v-if="inMaintenance">
-            <p>Name:{{ name }}</p>
-            <p>Capacity: {{ capacity }}</p>
-            <p>Date of build: {{ dateOfBuild }}</p>
-            <p>Maintenance frequency: {{ maintenanceFrequency }}</p>
-            <p>Duration: {{ duration }}</p>
-            <p>Minimum height: {{ minHeight }}</p>
-            <p>Speed: {{ speed }}</p>
-            <p>In maintenance: {{ inMaintenance }}</p>
-            <p>Categories: <span v-for="(category, index) in categories" :key="index">
-                    {{ category.title }},
-                </span></p>
-            <p>Previous maintenances: <span v-for="(maintenance, index) in maintenances" :key="index">
-                    {{ maintenance.reason }},
-                </span></p>
-            <p>Next maintenance due: Ongoing</p>
-            <a :href="onrideVideo" target="_blank">Onride video</a>
-            <img :src="image" :alt="name">
-            <br>
-            <br>
-            <RouterLink :to="{ path: '/attraction/edit', query: { name: name } }" class="btn">Edit Attraction
-            </RouterLink>
-            <button @click="endMaintenance" class="btn">End Maintenance</button>
-        </template>
-        <template v-else>
-            <p>Name:{{ name }}</p>
-            <p>Capacity: {{ capacity }}</p>
-            <p>Date of build: {{ dateOfBuild }}</p>
-            <p>Maintenance frequency: {{ maintenanceFrequency }}</p>
-            <p>Duration: {{ duration }}</p>
-            <p>Minimum height: {{ minHeight }}</p>
-            <p>Speed: {{ speed }}</p>
-            <p>In maintenance: {{ inMaintenance }}</p>
-            <p>Categories: <span v-for="(category, index) in categories" :key="index">
-                    {{ category.title }},
-                </span></p>
-            <p>Previous maintenances: <span v-for="(maintenance, index) in maintenances" :key="index">
-                    {{ maintenance.reason }},
-                </span></p>
-            <p>Next maintenance due: {{ nextMaintenance }}</p>
-            <a :href="onrideVideo" target="_blank">Onride video</a>
-            <img :src="image" :alt="name">
-            <br>
-            <br>
-            <RouterLink :to="{ path: '/attraction/edit', query: { name: name } }" class="btn">Edit Attraction
-            </RouterLink>
-            <RouterLink :to="{ path: '/maintenance/add', query: { name: name } }" class="btn">Start Maintenance
-            </RouterLink>
-        </template>
+      <p>Name: {{ name }}</p>
+      <p>Capacity: {{ capacity }}</p>
+      <p>Date of build: {{ dateOfBuild }}</p>
+      <p>Maintenance frequency: {{ maintenanceFrequency }}</p>
+      <p>Duration: {{ duration }}</p>
+      <p>Minimum height: {{ minHeight }}</p>
+      <p>Speed: {{ speed }}</p>
+      <p>In maintenance: {{ inMaintenance }}</p>
+      <p v-if="inMaintenance">Next maintenance due: Ongoing</p>
+      <p v-else>Next maintenance due: {{ nextMaintenance }}</p>
+      <p>Categories: 
+        <span v-for="(category, index) in categories" :key="index">{{ category.title }}</span>
+      </p>
+      <p>Previous maintenances: 
+        <span v-for="(maintenance, index) in maintenances" :key="index">{{ maintenance.reason }}</span>
+      </p>
+      <a :href="onrideVideo" target="_blank">Onride video</a>
+      <img :src="image" :alt="name">
+      <br><br>
+      <RouterLink :to="{ path: '/attraction/edit', query: { name: name } }" class="btn">Edit Attraction</RouterLink>
+      <template v-if="inMaintenance">
+        <button @click="endMaintenance" class="btn">End Maintenance</button>
+      </template>
+      <template v-else>
+        <RouterLink :to="{ path: '/maintenance/add', query: { name: name } }" class="btn">Start Maintenance</RouterLink>
+      </template>
     </div>
-</template>
+  </template>
